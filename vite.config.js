61dbcd19301d15710acaa6b5e2cb83379e2d5490
carlosDanielifst <carlos_daniel_spindler@hotmail.com>
2025-09-22 22:14:12 -0300

    Fix: Final project configuration for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // La propiedad 'base' le indica a Vite dónde se encuentra la aplicación en GitHub Pages.
-  // Es la URL de tu repositorio. Esto permite que el proceso de "build" encuentre todos los archivos correctamente.
-  base: '/Trafico-Redes/',
   plugins: [react()],
 })
