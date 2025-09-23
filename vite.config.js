378d7711a06b9366f2aa3f90c310e7cf60a7ee43
carlosDanielifst <carlos_daniel_spindler@hotmail.com>
2025-09-22 21:43:07 -0300

    Arreglo final cambio de ubicación del proyecto para deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // La propiedad 'base' le indica a Vite dónde se encuentra la aplicación en GitHub Pages.
+  // Es la URL de tu repositorio. Esto permite que el proceso de "build" encuentre todos los archivos correctamente.
+  base: '/Trafico-Redes/',
   plugins: [react()],
 })
